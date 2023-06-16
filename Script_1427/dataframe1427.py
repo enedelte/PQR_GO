b7e664bc8d4dc0f160c6513d280ece6baa4714d4
enedelte <daalejoar@gmail.com>
2023-06-16 16:34:14 -0500

    -m "16/06/2023"

--- Script_1427/dataframe1427.py
+++ Script_1427/dataframe1427.py
@@ -1,20 +1,11 @@
-import querygs 
-import gspread
+from querygs import plano 
 import pandas as pd
 import numpy as np
 
 #Application_Pandas
-print()
 
-certificados_1427 = pd.read_csv(r'C:\Users\dapache\python_scripts\myvenv\scripts_python\Script_1427\certificados.txt',
-                                 sep=' ',
-                                 index_col=0, 
-                                 header=0
-                                 )
+dataframe_plano = pd.DataFrame(plano)
 
-valores = pd.DataFrame(certificados_1427)
-
-print(valores)
-
+print(dataframe_plano)
 
 
